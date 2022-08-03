07331439a4555990a2f0910b047b80d7a75b02a4
Miguel R <miguel@debloat.us>
2022-08-03 12:21:07 -0400

    function composition

---
+++ Topics/Function composition/Disjunct all/src/Main.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.IntPredicate;
+import java.util.stream.Collectors;
+
+class CombiningPredicates {
+
+    /**
+     * The method represents a disjunct operator for a list of predicates.
+     * For an empty list it returns the always false predicate.
+     */
+    public static IntPredicate disjunctAll(List<IntPredicate> predicates) {
+        if (predicates.size() == 0) {
+            return x -> false;
+        }
+        int num = 0 ;
+        for (IntPredicate predicate : predicates) {
+            if (predicate.test(num)) {
+                return x -> true;
+            }
+        }
+
+        return x -> false;
+    }
+
+    // Don't change the code below
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String[] strings = scanner.nextLine().split(" ");
+
+        List<Boolean> values = Arrays.stream(strings)
+                .map(Boolean::parseBoolean)
+                .collect(Collectors.toList());
+
+        List<IntPredicate> predicates = new ArrayList<>();
+        values.forEach(v -> predicates.add(x -> v));
+
+        System.out.println(disjunctAll(predicates).test(0));
+    }
+}
